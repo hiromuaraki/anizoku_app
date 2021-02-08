ac7c903b983efecfb18d96edefc7d608aef7232d
hiromuaraki <shinji06011@gmail.com>
2021-02-08 15:24:38 +0900

    admin配下のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/admin', to: 'admin#index'
+  namespace :admin do
+    resources :posts
+  end
+
 end
