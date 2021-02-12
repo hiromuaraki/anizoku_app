d130c257da14e7bf0282efb31bf3dab1c6212cd3
hiromuaraki <shinji06011@gmail.com>
2021-02-12 10:44:00 +0900

    javascript各種ファイル設定

---
+++ app/assets/stylesheets/application.js
@@ -0,0 +1,8 @@
+//= require bootstrap/dist/css/bootstrap.min.css
+//= require rails-ujs
+//= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .
+// = require data-confirm-modal
