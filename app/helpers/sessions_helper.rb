728bec38c8a35d42f8bfd34aa0b2371a63061423
hiromuaraki <shinji06011@gmail.com>
2021-02-12 13:47:39 +0900

    cookiesを保存できるように変更 login時token発行ロジック追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,21 +1,53 @@
 module SessionsHelper
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif(user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        logged_in user
+        @current_user = user
+      end
+    end
+  end
+
+  #ログイン済みにする
   def logged_in(user)
     session[:user_id] = user.id
   end
 
+  #ログインしているか
   def logged_in?
     !!current_user
   end
 
-  def if_not_logged_in
-    render "" ,danger:"ログインしてください" if logged_in?
+  def if_not_logged_in?
+    redirect_to login_path ,danger:"ログインしてください" if logged_in?
+  end
+
+  def redirect_if_already_logged_in
+    if logged_in?
+      redirect_to static_pages_home_path, danger: "すでにログインしています"
+    end
+  end
+
+  #永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     current_user = nil
   end
