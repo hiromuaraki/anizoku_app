99362ac26843f3a1a51dbc799d275e89c3e64d7d
hiromuaraki <shinji06011@gmail.com>
2021-02-12 10:48:27 +0900

    ログアウトロジック追加 ヘッダーをsharedで管理できるように実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#join_anizoku'
-  
   namespace :static_pages do
     get :home
     get :privacy
   end
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
   
   get '/admin', to: 'admin#index'
   namespace :admin do
